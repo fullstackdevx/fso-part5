d2a6446ae3a07c9799611e6ffb04253015746ea8
“fsdx” <fullstackdevx@gmail.com>
2021-03-22 17:29:35 +0100

    bloglist frontend, step3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
@@ -22,6 +25,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -30,7 +34,9 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password })
 
+      blogService.setToken(user.token)
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+
       setUser(user)
       setUsername('')
       setPassword('')
@@ -49,19 +55,48 @@ const App = () => {
     window.localStorage.removeItem('loggedBlogappUser')
   }
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+
+    const returnedBlog = await blogService.create(blogObject)
+    setBlogs([...blogs, returnedBlog])
+
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+
   const loginForm = () => (
-    <div>
-      <h2>log in to application</h2>
-      <form onSubmit={handleLogin} >
-        <div>username
-          <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
-        </div>
-        <div>password
-          <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
+    <form onSubmit={handleLogin} >
+      <div>username
+        <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>password
+        <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>title
+        <input type="text" value={newTitle} name="Title" onChange={({ target }) => setNewTitle(target.value)}/>
+      </div>
+      <div>author
+        <input type="text" value={newAuthor} name="Author" onChange={({ target }) => setNewAuthor(target.value)} />
+      </div>
+      <div>url
+        <input type="text" value={newUrl} name="Url" onChange={({ target }) => setNewUrl(target.value)} />
+      </div>
+      <button type="submit">create</button>
+    </form>
   )
 
   return (
@@ -69,10 +104,19 @@ const App = () => {
       <Notification message={errorMessage} />
 
       {user === null
-        ? loginForm()
-        : (<div>
-          <h2>blogs</h2>
+        ? (
+        <div>
+          <h2>log in to application</h2>
+          {loginForm()}
+        </div>)
+        : (
+        <div>
+          <h1>blogs</h1>
           <p>{user.name} logged-in</p><button onClick={handleLogout}>logout</button>
+
+          <h2>create new</h2>
+          {blogForm()}
+
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
