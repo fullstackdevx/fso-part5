1a93cc1cf204ee4c63a8fed719204314b602a6f9
“fsdx” <fullstackdevx@gmail.com>
2021-03-25 12:48:15 +0100

    Blog list frontend, step11

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
   const [newTitle, setNewTitle] = useState('')
@@ -32,6 +33,10 @@ const BlogForm = ({ createBlog }) => {
       </div>
       <button type="submit">create</button>
     </form>)
+}
+
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired
 }
 
 export default BlogForm
