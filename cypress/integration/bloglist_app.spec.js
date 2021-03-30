7f862e196034985997aa4c8c7c03a577b4a36277
“fsdx” <fullstackdevx@gmail.com>
2021-03-30 16:46:37 +0200

    bloglist end to end testing, step2

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,18 @@
 describe('bloglist app',  function()  {
   beforeEach(function() {
+    //reset users and blogs
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    //creates a user
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'secret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
+    //opens the web
     cy.visit('http://localhost:3000')
   })
 
@@ -12,5 +24,24 @@ describe('bloglist app',  function()  {
     cy.get('#username')
     cy.get('#password')
     cy.get('#login-button')
+  })
+
+  describe('login', function () {
+    it('succeeds with correct credentials', function (){
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('secret')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged-in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').should('contain', 'Wrong credentials')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
